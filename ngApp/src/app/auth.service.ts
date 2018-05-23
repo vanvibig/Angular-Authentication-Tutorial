0e91d64212b00c1a6baa8fb2fef8ecf5a91805de
Jiéverson Maissiat <jieverson@gmail.com>
2018-05-23 10:41:43 +0700

    Angular Authentication Tutorial - 27 - Implementing logout

--- ngApp/src/app/auth.service.ts
+++ ngApp/src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthService {
@@ -7,27 +8,30 @@ export class AuthService {
   private _registerUrl = "http://localhost:3000/api/register";
   private _loginUrl = "http://localhost:3000/api/login";
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private _router:Router) {
   }
 
-  registerUser(user){
+  registerUser(user) {
     return this.http.post<any>(this._registerUrl, user);
   }
 
-  loginUser(user){
+  loginUser(user) {
     return this.http.post<any>(this._loginUrl, user);
   }
 
-  loggedIn(){
+  loggedIn() {
     /**
      * !! is return true if * exist, else return false
      */
     return !!localStorage.getItem('token');
   }
 
-  getToken(){
-    return localStorage.getItem('token');
+  logoutUser() {
+    localStorage.removeItem('token');
+    this._router.navigate(['events']);
   }
 
-
+  getToken() {
+    return localStorage.getItem('token');
+  }
 }
