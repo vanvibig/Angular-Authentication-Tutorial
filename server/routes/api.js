7991170a0bc6d341a5ba238aaf1661613e62760c
Jiéverson Maissiat <jieverson@gmail.com>
2018-05-23 09:26:04 +0700

    Angular Authentication Tutorial - 23 - Storing JWT in the front-end

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const User = require('../models/user');
 const mongoose = require('mongoose');
 const db = "mongodb://127.0.0.1:27017/eventsdb";
 
 mongoose.connect(db, err => {
-    if(err)
+    if (err)
         console.log('Error' + err);
     else
         console.log('Connected to mongodb');
@@ -19,25 +20,32 @@ router.post('/register', (req, res) => {
     let userData = req.body;
     let user = new User(userData);
     user.save((error, registeredUser) => {
-        if(error)
+        if (error)
             console.log(error);
-        else
-            res.status(200).send(registeredUser);
+        else {
+            let payload = {subject: registeredUser._id};
+            let token = jwt.sign(payload, 'secretKey');
+            res.status(200).send({token});
+        }
     });
 });
 
 router.post('/login', (req, res) => {
     let userData = req.body;
 
-    User.findOne({email: userData.email}, (error, user)=>{
-        if(error)
+    User.findOne({email: userData.email}, (error, user) => {
+        if (error)
             console.log(error);
-        else{
-            if(!user)
+        else {
+            if (!user)
                 res.status(404).send('Invalid email');
             else if (user.password !== userData.password)
                 res.status(401).send('Invalid password');
-            else res.status(200).send(user);
+            else {
+                let payload = {subject: user._id};
+                let token = jwt.sign(payload, 'secretKey');
+                res.status(200).send({token});
+            }
         }
     });
 });
@@ -49,27 +57,27 @@ router.get('/events', (req, res) => {
             "name": "Auto Expo",
             "description": "Lorem ipsum",
             "date": "2018-05-22"
-        },{
+        }, {
             "_id": 2,
             "name": "Auto Expo",
             "description": "Lorem ipsum",
             "date": "2018-05-22"
-        },{
+        }, {
             "_id": 3,
             "name": "Auto Expo",
             "description": "Lorem ipsum",
             "date": "2018-05-22"
-        },{
+        }, {
             "_id": 4,
             "name": "Auto Expo",
             "description": "Lorem ipsum",
             "date": "2018-05-22"
-        },{
+        }, {
             "_id": 5,
             "name": "Auto Expo",
             "description": "Lorem ipsum",
             "date": "2018-05-22"
-        },{
+        }, {
             "_id": 6,
             "name": "Auto Expo",
             "description": "Lorem ipsum",
@@ -86,27 +94,27 @@ router.get('/special', (req, res) => {
             "name": "Auto Expo",
             "description": "Lorem ipsum",
             "date": "2018-05-22"
-        },{
+        }, {
             "_id": 2,
             "name": "Auto Expo",
             "description": "Lorem ipsum",
             "date": "2018-05-22"
-        },{
+        }, {
             "_id": 3,
             "name": "Auto Expo",
             "description": "Lorem ipsum",
             "date": "2018-05-22"
-        },{
+        }, {
             "_id": 4,
             "name": "Auto Expo",
             "description": "Lorem ipsum",
             "date": "2018-05-22"
-        },{
+        }, {
             "_id": 5,
             "name": "Auto Expo",
             "description": "Lorem ipsum",
             "date": "2018-05-22"
-        },{
+        }, {
             "_id": 6,
             "name": "Auto Expo",
             "description": "Lorem ipsum",
